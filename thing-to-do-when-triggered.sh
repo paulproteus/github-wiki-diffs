dcee6306d66fef7a057c7f338ba12752c45cfa77
Asheesh Laroia <asheesh@asheesh.org>
2015-01-07 10:29:23 -0800

    wip

--- thing-to-do-when-triggered.sh
+++ thing-to-do-when-triggered.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+KEYWORD_FOR_SANITY_CHECKING_SOURCE_ORIGIN="github"
 SOURCE_REPO_DIR="fake-source-repo"
 DEST_REPO_DIR="fake-dest-repo"
 
@@ -15,4 +16,23 @@ DEST_REPO_DIR="fake-dest-repo"
 #   cds into a repo called source-repo, gets the latest data
 #   from github, and then pushes that to dest-repo.
 #   The real logic for sending out emails lives in dest-repo.
-#
+pushd "$SOURCE_REPO_DIR"
+
+# Make sure there is a remote called origin; if not, bail now.
+git remote -v | grep -q "origin"
+
+# Make sure the remote called origin points at what we expect
+# to be origin.
+git remote -v | grep -q "$KEYWORD_FOR_SANITY_CHECKING_SOURCE_ORIGIN"
+
+# Make sure there is a remote called dest; if not, bail now.
+git remote -v | grep -q "dest"
+
+# Make sure the remote called dest points at DEST_REPO_DIR.
+git remote -v | grep -q "$(basename $DEST_REPO_DIR")
+
+# OK, great.
+git fetch origin
+git push dest origin/master:master
+
+# And now, the magic should have happened.
